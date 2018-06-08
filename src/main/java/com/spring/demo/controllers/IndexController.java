6c5e03ecfeb13c9243a53ba346cdeb88985061f8
Javier Delgado <packo6300@gmail.com>
2018-06-08 14:31:45 -0600

    first commit

---
+++ src/main/java/com/spring/demo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/index")
+	public String index(Model model){
+		model.addAttribute("titulo","hola desde controlador");
+		return "index";
+	}
+}
